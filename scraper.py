732f552ee73c5dd2c58ea69b9a265f763c3237ce
SelectSoft <52114175+SelectSoft@users.noreply.github.com>
2019-11-21 21:25:40 +0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -26,7 +26,7 @@
 import sqlite3
 import pandas as pd
 
-conn = sqlite3.connect("selectfashion3.sqlite")
+conn = sqlite3.connect("selectfashion4.sqlite")
 conn2 = sqlite3.connect("data.sqlite")
 
 images = pd.read_sql("Select * from images",conn)
